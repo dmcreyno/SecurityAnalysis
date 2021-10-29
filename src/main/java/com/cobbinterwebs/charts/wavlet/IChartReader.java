ec7a463899d87f25e58145c7ef12f6aaa26e325c
David McReynolds <david.mcreynolds@gmail.com>
2021-10-29 11:55:59 -0400

    formatting

--- src/main/java/com/cobbinterwebs/charts/wavlet/IChartReader.java
+++ src/main/java/com/cobbinterwebs/charts/wavlet/IChartReader.java
@@ -1,5 +1,32 @@
 package com.cobbinterwebs.charts.wavlet;
 
-public interface IChartReader{
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import com.cobbinterwebs.trades.config.Configuration;
+
+public interface IChartReader {
+	@SuppressWarnings("unchecked")
+	static IChartReader create(File pFile, Configuration pConfig) throws Error {
+		String className = Configuration.getInstance().getTradeChartType();
+		IChartReader rVal;
+		@SuppressWarnings("rawtypes")
+		Class clazz;
+		try {
+			clazz = Class.forName(className);
+			
+			try {
+			@SuppressWarnings("rawtypes") Constructor ctor = clazz.getConstructor(File.class, Configuration.class);
+			rVal = (IChartReader) ctor.newInstance(pFile,pConfig);
+			} catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+				throw new Error(e);
+			}
+		} catch (ClassNotFoundException e) {
+			throw new Error(e);
+		}
+		
+		return rVal;
+	}
 
 }
